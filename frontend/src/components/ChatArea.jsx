34fd99157823a59c2e6a1cd3aabd6181b8f8d9dd
Shanid P <pazhayangadi2006@gmail.com>
2026-06-12 04:18:09 +0530

    online offline issue solved

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -230,7 +230,7 @@ const [text, setText] = useState("");
         }
 
 
-        if(data.OuserID && contact.status != "Online"){
+        if(data.OuserID && status != "Online"){
           const response = await fetch(`${API_URL}/user-data?chat_id=${encodeURIComponent(chat_id)}`, {
           method: "GET",
           headers: {
